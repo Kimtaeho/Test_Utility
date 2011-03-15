7e177b1c88f090cd9bb786d75200cbe3d227d7d2
night <dhvkcy21@nate.com>
2011-03-15 12:22:33 +0900

    test util - 2

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC		= gcc
-CFLAGS		= -W -Wall -O2
+CFLAGS		= -W -Wall -O2 -g
 LDFLAGS		= -lm
 
 like : like.c love.c
--- like.c
+++ like.c
@@ -1,10 +1,29 @@
+#include <libintl.h>
 #include <stdio.h>
+
+#define PACKAGE	    "like"
+#define LOCALEDIR   "/home/night/Test_Utility"
 
 void love();
 
+char *getname()
+{
+    static char name[128];
+    
+    printf( gettext("Input name : "));
+    scanf("%s", name);
+    return name;
+}
+
 int main ()
 {
-    printf("hello\n");
-    love();
+    char *str;
+    setlocale( LC_ALL, " ");
+    bindtextdomain( PACKAGE, LOCALEDIR);
+    textdomain( PACKAGE );
+    str = getname();
+    printf( gettext(" hello \n"));
+    printf("%s\n",str);
+    //love();
     return 0;
 }
